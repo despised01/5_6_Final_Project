f67bef645570461a63fd740ec557f2d84aef8519
Антон Левин <despised01@gmail.com>
2022-11-16 22:49:51 +0300

    Добавлен метод для совершения опроса

--- 5_6_Final_Project/Program.cs
+++ 5_6_Final_Project/Program.cs
@@ -4,9 +4,10 @@ namespace _5_6_Final_Project
 {
     class Program
     {
-        static (string FirstName, string LastName, int Age, bool HasPet, int CountPet, string[] NamePet, string[] FavColors) UserInfo()
+
+        static (string FirstName, string LastName, int Age, bool HasPet, int CountPet, string[] NamePet, int CountColors, string[] FavColors) UserInfo()
         {
-            (string FirstName, string LastName, int Age, bool HasPet, int CountPet, string[] NamePet, string[] FavColors) User;
+            (string FirstName, string LastName, int Age, bool HasPet, int CountPet, string[] NamePet, int CountColors, string[] FavColors) User;
 
             Console.WriteLine("Здравствуйте! Давайте уточним ваши данные.");
 
@@ -16,15 +17,73 @@ namespace _5_6_Final_Project
             Console.WriteLine("Напишите вашу фамилию:");
             User.LastName = Console.ReadLine();
 
-            Console.WriteLine("Напишите ваш возраст:");
-            User.Age = int.Parse(Console.ReadLine());
+            string age;
+            int intage;
+            do
+            {
+                Console.WriteLine("Напишите ваш возраст цифрами:");
+                age = Console.ReadLine();
+            }
+            while (CheckNum(age, out intage));
+
+            User.Age = intage;
 
             Console.WriteLine("Есть ли у вас питомцы? Ответ да или нет");
-            User.HasPet = bool.Parse(Console.ReadLine());
+            var Answer = Console.ReadLine();
 
-            if(User.HasPet = "да")
+            if (Answer == "да")
+                User.HasPet = true;
+            else
+                User.HasPet = false;
 
+            if(User.HasPet == true)
+            {
+                Console.WriteLine("Сколько у вас питомцев? Введите пожалуйста ответ цифрами.");
+                User.CountPet = int.Parse(Console.ReadLine());
+                int num = User.CountPet;
 
+                User.NamePet = new string[num];
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine("\nВведите кличку {0} питомца:", i + 1);
+                    User.NamePet[i] = Console.ReadLine();
+                }
+
+            }
+
+            Console.WriteLine("Сколько у вас любимых цветов? Введите пожалуйста ответ цифрами.");
+            User.CountColors = int.Parse(Console.ReadLine());
+            int colornum = User.CountColors;
+
+            User.FavColors = new string[colornum];
+            for (int j = 0; j < colornum; j++)
+            {
+                Console.WriteLine("\nВведите ваш любимый цвет {0}", j + 1);
+                User.FavColors[j] = Console.ReadLine();
+            }
+
+            return User;
         }
+
+        static bool CheckNum(string number, out int corrnumber)
+        {
+            if (int.TryParse(number, out int intage))
+            {
+                if (intage > 0)
+                {
+                    corrnumber = intage;
+                    return false;
+                }
+            }
+            {
+                corrnumber = 0;
+                return true;
+            }
+        }
+
+       
+
+        
+
     }
 }
