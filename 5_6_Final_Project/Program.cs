7ecdb7901754d1ca50b3d7cc0cb2625982cdfe5c
Антон Левин <despised01@gmail.com>
2022-11-16 20:57:55 +0300

    Назначен метод для анкетирования

--- 5_6_Final_Project/Program.cs
+++ 5_6_Final_Project/Program.cs
@@ -4,9 +4,27 @@ namespace _5_6_Final_Project
 {
     class Program
     {
-        static void Main(string[] args)
+        static (string FirstName, string LastName, int Age, bool HasPet, int CountPet, string[] NamePet, string[] FavColors) UserInfo()
         {
-            Console.WriteLine("Hello World!");
+            (string FirstName, string LastName, int Age, bool HasPet, int CountPet, string[] NamePet, string[] FavColors) User;
+
+            Console.WriteLine("Здравствуйте! Давайте уточним ваши данные.");
+
+            Console.WriteLine("Напишите ваше имя:");
+            User.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Напишите вашу фамилию:");
+            User.LastName = Console.ReadLine();
+
+            Console.WriteLine("Напишите ваш возраст:");
+            User.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Есть ли у вас питомцы? Ответ да или нет");
+            User.HasPet = bool.Parse(Console.ReadLine());
+
+            if(User.HasPet = "да")
+
+
         }
     }
 }
