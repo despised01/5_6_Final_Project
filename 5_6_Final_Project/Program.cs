68632f99dc3d78b37953037b4c37ed71b05b5698
Антон Левин <despised01@gmail.com>
2022-11-16 19:52:04 +0300

    Добавьте файлы проекта.

---
+++ 5_6_Final_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5_6_Final_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
